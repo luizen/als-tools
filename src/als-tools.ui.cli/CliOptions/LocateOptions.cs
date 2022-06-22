52a8289bcec39868cee032d55754c16e2a8470b4
luizen <zenluiz@gmail.com>
2022-06-22 08:05:03 -0300

    Renamed option

--- src/als-tools.ui.cli/CliOptions/LocateOptions.cs
+++ src/als-tools.ui.cli/CliOptions/LocateOptions.cs
@@ -6,7 +6,7 @@ namespace AlsTools.Ui.Cli.CliOptions;
 public class LocateOptions : CommonOptions
 {
     [Option("plugin-names", Group = "locate options", Min = 1, HelpText = "The plugin names to locate projects by.")]
-    public IReadOnlyCollection<string>? PluginsToLocate { get; set; }
+    public IReadOnlyCollection<string>? PluginNamesToLocate { get; set; }
 
     [Option("plugin-formats", Group = "locate options", Min = 1, HelpText = "The plugin formats to locate projects by.")]
     public IReadOnlyCollection<PluginFormat>? PluginFormatsToLocate { get; set; }
