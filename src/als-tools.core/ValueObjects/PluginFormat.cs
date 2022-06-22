06afc21c2fca8de3c2fbf72b3d7bedbada54b816
luizen <zenluiz@gmail.com>
2022-06-22 08:03:21 -0300

    Adding LocateOptions plugin-formats

--- src/als-tools.core/ValueObjects/PluginFormat.cs
+++ src/als-tools.core/ValueObjects/PluginFormat.cs
@@ -3,7 +3,7 @@ namespace AlsTools.Core.ValueObjects;
 public enum PluginFormat
 {
     AU = 1,
-    
+
     VST2 = 2,
 
     VST3 = 3
--- src/als-tools.ui.cli/CliOptions/LocateOptions.cs
+++ src/als-tools.ui.cli/CliOptions/LocateOptions.cs
@@ -1,8 +1,13 @@
+using AlsTools.Core.ValueObjects;
+
 namespace AlsTools.Ui.Cli.CliOptions;
 
 [Verb("locate", HelpText = "Locates projects containing given plugins by their names.")]
 public class LocateOptions : CommonOptions
 {
-    [Option("plugin-names", Required = true, Min = 1, HelpText = "The plugin names to locate projects by.")]
-    public IReadOnlyCollection<string> PluginsToLocate { get; set; } = new List<string>();
+    [Option("plugin-names", Group = "locate options", Min = 1, HelpText = "The plugin names to locate projects by.")]
+    public IReadOnlyCollection<string>? PluginsToLocate { get; set; }
+
+    [Option("plugin-formats", Group = "locate options", Min = 1, HelpText = "The plugin formats to locate projects by.")]
+    public IReadOnlyCollection<PluginFormat>? PluginFormatsToLocate { get; set; }
 }
