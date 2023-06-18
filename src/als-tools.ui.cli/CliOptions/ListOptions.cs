d06ac338c8cc8b79af48b311599a37e0af76f9a2
Luiz Zen <zenluiz@gmail.com>
2023-06-18 09:32:18 -0300

    Plugin name only - remaining

--- src/als-tools.ui.cli/CliOptions/ListOptions.cs
+++ src/als-tools.ui.cli/CliOptions/ListOptions.cs
@@ -1,7 +1,11 @@
 namespace AlsTools.Ui.Cli.CliOptions;
 
-[Verb("list", HelpText = "List all projects stored in the als-tools database.")]
+[Verb("list", HelpText = "List all projects, or plugin names only, stored in the als-tools database.")]
 public class ListOptions : CommonOptions
 {
+
+    [Option("plugin-names-only", HelpText = "Whether to only display the plugin names found in all projects", Default = false)]
+    public bool PluginNamesOnly { get; set; }
+
     public override bool IsEmpty => false;
 }
