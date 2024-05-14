0ec173fa289b9016b049b913ddbfc785579da439
Luiz Zen <zenluiz@gmail.com>
2024-05-13 23:06:08 -0300

    Project fields

--- src/als-tools.core/Models/Project.cs
+++ src/als-tools.core/Models/Project.cs
@@ -2,13 +2,69 @@
 
 public partial class Project
 {
+    /// <summary>
+    /// Internal (persistency related) project ID.
+    /// </summary>
     public int Id { get; set; }
 
-    public string Name { get; set; } = null!;
+    /// <summary>
+    /// The project name
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
 
-    public string? Path { get; set; }
+    /// <summary>
+    /// The full path of this project
+    /// </summary>
+    public string Path { get; set; } = string.Empty;
 
-    public int? Tempo { get; set; }
 
-    public virtual ICollection<Track> Tracks { get; set; } = new List<Track>();
+    /// <summary>
+    /// Creator ("Live version")
+    /// </summary>
+    public string Creator { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Minor version
+    /// </summary>
+    public string MinorVersion { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Major version
+    /// </summary>
+    public string MajorVersion { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Schema change count
+    /// </summary>
+    public int? SchemaChangeCount { get; set; }
+
+    /// <summary>
+    /// The project tempo/BPM.
+    /// </summary>
+    public double? Tempo { get; set; }
+
+    /// <summary>
+    /// The project time signature. It is probably a bit mask.
+    /// </summary>
+    public int? TimeSignature { get; set; }
+
+    /// <summary>
+    /// The project global groove amount
+    /// </summary>
+    public double? GlobalGrooveAmount { get; set; }
+
+    /// <summary>
+    /// The tracks this project contains
+    /// </summary>
+    public virtual ICollection<Track> Tracks { get; set; } = [];
+
+    /// <summary>
+    /// The scenes this project contains
+    /// </summary>
+    public virtual ICollection<Scene> Scenes { get; set; }
+
+    /// <summary>
+    /// The locators this project contains
+    /// </summary>
+    public virtual ICollection<Locator> Locators { get; set; }
 }
