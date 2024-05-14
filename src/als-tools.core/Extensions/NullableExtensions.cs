8603c9723128ac959d1099f01a22856bf65ec438
Luiz Zen <zenluiz@gmail.com>
2024-05-14 07:48:30 -0300

    Extensions

---
+++ src/als-tools.core/Extensions/NullableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace AlsTools.Core.Extensions;
+
+public static class NullableExtensions
+{
+    public static bool HasValueTrue(this bool? instance)
+    {
+        return instance.HasValue && instance.Value;
+    }
+
+}
